4a05afa22e2b8c9ada4cb413c7edd8ce97f59b47
kstdbrv <kstdbrv1@gmail.com>
2020-05-18 18:29:42 +0300

    change3 section advanteges

--- js/main.js
+++ js/main.js
@@ -8,8 +8,12 @@ $(function(){
 
   $('select').styler();
 
-  $('.header__btn-menu').on('click', function(){
-    $('.menu ul').slideToggle();
-  });
+
+
+
+
+
+  //$('select').styler('destroy');
+  
 
 });
